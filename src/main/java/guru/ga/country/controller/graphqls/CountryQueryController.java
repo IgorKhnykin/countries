75bcb076a0aeb977d1ce99dc89677c8072e1ffc4
IgorKhnykin <igor.khnykin@mail.ru>
2025-05-28 08:37:47 +0300

    HW-GRPC

--- src/main/java/guru/ga/country/controller/graphqls/CountryQueryController.java
+++ src/main/java/guru/ga/country/controller/graphqls/CountryQueryController.java
@@ -1,18 +1,13 @@
 package guru.ga.country.controller.graphqls;
 
-import guru.ga.country.model.CountryJson;
 import guru.ga.country.model.gql.CountryGql;
 import guru.ga.country.service.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
 import org.springframework.graphql.data.method.annotation.Argument;
-import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Controller
 public class CountryQueryController {
